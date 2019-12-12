67f6cd8040738d647ad937299cf5d13dfdfaeeea
Afonso Escudeiro <afonso_escudeiro@hotmail.com>
2019-12-12 18:03:53 -0100

    added web.xml

--- src/main/java/org/academiadecodigo/thunderstructs/controller/web/ReviewController.java
+++ src/main/java/org/academiadecodigo/thunderstructs/controller/web/ReviewController.java
@@ -2,13 +2,19 @@ package org.academiadecodigo.thunderstructs.controller.web;
 
 import org.academiadecodigo.thunderstructs.converters.ReviewDtoToReview;
 import org.academiadecodigo.thunderstructs.converters.ReviewToReviewDto;
+import org.academiadecodigo.thunderstructs.dto.ReviewDTO;
 import org.academiadecodigo.thunderstructs.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping ( "/review" )
+import java.util.List;
+
+@RestController
+@RequestMapping ( "/api/review" )
 public class ReviewController {
 
 	private ReviewService reviewService;
@@ -30,5 +36,12 @@ public class ReviewController {
 		this.reviewToReviewDto = reviewToReviewDto;
 	}
 
+	@RequestMapping ( method = RequestMethod.GET )
+	public ResponseEntity< List< ReviewDTO > > listReviews ( String name ) {
+		if(reviewService.list( name ) == null ){
+			return new ResponseEntity<>( HttpStatus.NOT_FOUND );
+		}
+		return new ResponseEntity<>( reviewToReviewDto.convertList( reviewService.list( name ) ) , HttpStatus.OK);
+	}
 
 }
