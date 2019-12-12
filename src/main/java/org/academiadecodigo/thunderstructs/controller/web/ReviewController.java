09242132d524d701947b4506854e632d91e6990f
Afonso Escudeiro <afonso_escudeiro@hotmail.com>
2019-12-12 14:08:56 -0100

    Started reviewcontorller

--- src/main/java/org/academiadecodigo/thunderstructs/controller/web/ReviewController.java
+++ src/main/java/org/academiadecodigo/thunderstructs/controller/web/ReviewController.java
@@ -1,4 +1,34 @@
 package org.academiadecodigo.thunderstructs.controller.web;
 
+import org.academiadecodigo.thunderstructs.converters.ReviewDtoToReview;
+import org.academiadecodigo.thunderstructs.converters.ReviewToReviewDto;
+import org.academiadecodigo.thunderstructs.services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping ( "/review" )
 public class ReviewController {
+
+	private ReviewService reviewService;
+	private ReviewDtoToReview reviewDtoToReview;
+	private ReviewToReviewDto reviewToReviewDto;
+
+	@Autowired
+	public void setReviewService ( ReviewService reviewService ) {
+		this.reviewService = reviewService;
+	}
+
+	@Autowired
+	public void setReviewDtoToReview ( ReviewDtoToReview reviewDtoToReview ) {
+		this.reviewDtoToReview = reviewDtoToReview;
+	}
+
+	@Autowired
+	public void setReviewToReviewDto ( ReviewToReviewDto reviewToReviewDto ) {
+		this.reviewToReviewDto = reviewToReviewDto;
+	}
+
+
 }
