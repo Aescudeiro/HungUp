90a10786e12ad2b4bb24635e315c4ed6fb0918f6
Fabio Silva <fsilva_1992@hotmail.com>
2019-12-12 15:39:02 -0100

    Added implementation to converters

--- src/main/java/org/academiadecodigo/thunderstructs/dto/CompanyDTO.java
+++ src/main/java/org/academiadecodigo/thunderstructs/dto/CompanyDTO.java
@@ -1,4 +1,34 @@
 package org.academiadecodigo.thunderstructs.dto;
 
+import java.util.List;
+
 public class CompanyDTO {
+
+    private Integer id;
+    private String name;
+    private List<ReviewDTO> reviews;
+
+    public void setReviews(List<ReviewDTO> reviews) {
+        this.reviews = reviews;
+    }
+
+    public List<ReviewDTO> getReviews() {
+        return reviews;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
